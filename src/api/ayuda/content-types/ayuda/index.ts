5f9e84acee76bf001f7ed1635a8fdf1090fbb127
yoston <yostoncornet@gmail.com>
2026-04-24 15:51:49 -0500

    test

--- src/api/ayuda/content-types/ayuda/index.ts
+++
@@ -1,9 +0,0 @@
-import schema from './schema';
-
-export default {
-    collectionName: schema.collectionName,
-    info: schema.info,
-    options: schema.options,
-    pluginOptions: schema.pluginOptions,
-    attributes: schema.attributes,
-};
